439a6558cdc790a4ca62a473167f91fb02ec8fe7
Spencer-Lochne <spencer.lochne@gmail.com>
2025-01-05 21:12:57 -0500

    5i Solution

--- 2-copy-of-code/lesson-05/chatbot-project/src/components/ChatMessages.jsx
+++ 2-copy-of-code/lesson-05/chatbot-project/src/components/ChatMessages.jsx
@@ -19,6 +19,7 @@ function ChatMessages({ chatMessages }) {
           <ChatMessage
             message={chatMessage.message}
             sender={chatMessage.sender}
+            time={chatMessage.time}
             key={chatMessage.id}
           />
         );
