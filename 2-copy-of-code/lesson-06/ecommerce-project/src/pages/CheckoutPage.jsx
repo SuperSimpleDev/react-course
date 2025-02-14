b5b48c23c13a5677ac6856063451e0ab46ac3f61
Spencer Lochne <spencer.lochne@gmail.com>
2025-02-13 19:38:46 -0500

    6a Solution

--- 2-copy-of-code/lesson-06/ecommerce-project/src/pages/CheckoutPage.jsx
+++ 2-copy-of-code/lesson-06/ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import './checkout-header.css';
+import { CheckoutHeader } from './CheckoutHeader';
 import './CheckoutPage.css';
 
 export function CheckoutPage() {
@@ -6,25 +6,7 @@ export function CheckoutPage() {
     <>
       <title>Checkout</title>
 
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <a href="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (<a className="return-to-home-link"
-              href="/">3 items</a>)
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
+      <CheckoutHeader />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
