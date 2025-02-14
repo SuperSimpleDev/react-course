67209ae7da11822fe7e055967c0de2d9371d47b1
Spencer Lochne <spencer.lochne@gmail.com>
2025-02-13 20:39:45 -0500

    6f Solution

--- 2-copy-of-code/lesson-06/ecommerce-project/src/components/Header.jsx
+++ 2-copy-of-code/lesson-06/ecommerce-project/src/components/Header.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router';
+import { NavLink } from 'react-router';
 import './header.css';
 
 export function Header() {
   return (
     <div className="header">
       <div className="left-section">
-        <Link to="/" className="header-link">
+        <NavLink to="/" className="header-link">
           <img className="logo"
             src="images/logo-white.png" />
           <img className="mobile-logo"
             src="images/mobile-logo-white.png" />
-        </Link>
+        </NavLink>
       </div>
 
       <div className="middle-section">
@@ -22,16 +22,16 @@ export function Header() {
       </div>
 
       <div className="right-section">
-        <Link className="orders-link header-link" to="/orders">
+        <NavLink className="orders-link header-link" to="/orders">
 
           <span className="orders-text">Orders</span>
-        </Link>
+        </NavLink>
 
-        <Link className="cart-link header-link" to="/checkout">
+        <NavLink className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">3</div>
           <div className="cart-text">Cart</div>
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
