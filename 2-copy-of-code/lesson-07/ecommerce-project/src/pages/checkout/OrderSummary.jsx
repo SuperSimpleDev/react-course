3fb198e78b14b123a3f2aca708890717384a5750
Spencer Lochne <spencer.lochne@gmail.com>
2025-04-12 19:49:11 -0400

    7c Solution

--- 2-copy-of-code/lesson-07/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ 2-copy-of-code/lesson-07/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,21 +1,14 @@
-import dayjs from 'dayjs';
 import { CartItemDetails } from './CartItemDetails';
 import { DeliveryOptions } from './DeliveryOptions';
+import { DeliveryDate } from './DeliveryDate';
 
 export function OrderSummary({ cart, deliveryOptions }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
-        const selectedDeliveryOption = deliveryOptions
-          .find((deliveryOption) => {
-            return deliveryOption.id === cartItem.deliveryOptionId;
-          });
-
         return (
           <div key={cartItem.productId} className="cart-item-container">
-            <div className="delivery-date">
-              Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-            </div>
+            <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
 
             <div className="cart-item-details-grid">
               <CartItemDetails cartItem={cartItem} />
