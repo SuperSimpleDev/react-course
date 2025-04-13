ab19ed46eb166d8ac7716a0c7839571daf6f90ea
Spencer Lochne <spencer.lochne@gmail.com>
2025-04-12 20:12:31 -0400

    7d Solution Part 1

--- 2-copy-of-code/lesson-07/ecommerce-project/src/main.jsx
+++ 2-copy-of-code/lesson-07/ecommerce-project/src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  // Temporarily remove <StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
