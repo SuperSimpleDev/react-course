11f142a47b04af870b990daa5589107d122f3026
Spencer Lochne <spencer.lochne@gmail.com>
2025-04-13 16:01:17 -0400

    7j Solution

--- 2-copy-of-code/lesson-07/ecommerce-project/src/App.jsx
+++ 2-copy-of-code/lesson-07/ecommerce-project/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
-      <Route path="tracking" element={<TrackingPage cart={cart} />} />
+      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
     </Routes>
   )
 }
