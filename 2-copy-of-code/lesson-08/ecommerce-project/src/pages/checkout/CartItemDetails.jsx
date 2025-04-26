398edbd9e41294442c4cb610caed792ff61d6ca0
Spencer Lochne <spencer.lochne@gmail.com>
2025-04-26 15:11:03 -0400

    8f Solution Part 1

---
+++ 2-copy-of-code/lesson-08/ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { formatMoney } from '../../utils/money';
+
+export function CartItemDetails({ cartItem, loadCart }) {
+  const deleteCartItem = async () => {
+    await axios.delete(`/api/cart-items/${cartItem.productId}`);
+    await loadCart();
+  };
+
+  return (
+    <>
+      <img className="product-image"
+        src={cartItem.product.image} />
+
+      <div className="cart-item-details">
+        <div className="product-name">
+          {cartItem.product.name}
+        </div>
+        <div className="product-price">
+          {formatMoney(cartItem.product.priceCents)}
+        </div>
+        <div className="product-quantity">
+          <span>
+            Quantity: <input type="number" className="quantity-textbox" />
+            <span className="quantity-label">{cartItem.quantity}</span>
+          </span>
+          <span className="update-quantity-link link-primary">
+            Update
+          </span>
+          <span className="delete-quantity-link link-primary"
+            onClick={deleteCartItem}>
+            Delete
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
