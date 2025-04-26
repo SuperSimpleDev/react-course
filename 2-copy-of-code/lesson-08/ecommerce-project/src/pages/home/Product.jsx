2d1eb886f44e0f9b89084cb2b2dc35bef4cb5c05
Spencer Lochne <spencer.lochne@gmail.com>
2025-04-26 14:39:48 -0400

    8d Solution

--- 2-copy-of-code/lesson-08/ecommerce-project/src/pages/home/Product.jsx
+++ 2-copy-of-code/lesson-08/ecommerce-project/src/pages/home/Product.jsx
@@ -4,6 +4,7 @@ import { formatMoney } from '../../utils/money';
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
+  const [showAddedMessage, setShowAddedMessage] = useState(false);
 
   const addToCart = async () => {
     await axios.post('/api/cart-items', {
@@ -11,6 +12,12 @@ export function Product({ product, loadCart }) {
       quantity
     });
     await loadCart();
+
+    setShowAddedMessage(true);
+
+    setTimeout(() => {
+      setShowAddedMessage(false);
+    }, 2000);
   };
 
   const selectQuantity = (event) => {
@@ -58,7 +65,9 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart" style={{
+        opacity: showAddedMessage ? 1 : 0,
+      }}>
         <img src="images/icons/checkmark.png" />
         Added
       </div>
