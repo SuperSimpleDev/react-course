a9db6b30480dcb3a757cd9ebbda567ffd7698d3d
Spencer Lochne <spencer.lochne@gmail.com>
2025-04-26 14:02:15 -0400

    8c Solution

--- 2-copy-of-code/lesson-08/ecommerce-project/src/App.jsx
+++ 2-copy-of-code/lesson-08/ecommerce-project/src/App.jsx
@@ -6,6 +6,23 @@ import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
 import './App.css'
 
+// This makes axios available in the Console.
+// - Then, you can try running axios.post('/api/reset') in the Console.
+window.axios = axios;
+
+// More details:
+// - Normally, we can't access values (like axios) outside of a file.
+// - However, JavaScript has a built-in, global object called window
+//   (this represents the browser window).
+// - So one way to make a value accessible anywhere (including in the
+//   Console), is to attach it to the window object. That's why we
+//   do window.axios = axios;
+// - Now, in the Console, we can run window.axios.post(...)
+// - And JavaScript has another shortcut we can use. If we just type
+//   "axios", this is a shortcut for "window.axios"
+// - That's why the code window.axios = axios; lets us use "axios"
+//   anywhere (including in the Conosle).
+
 function App() {
   const [cart, setCart] = useState([]);
 
