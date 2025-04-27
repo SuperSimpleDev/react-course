50d1397e3dc73d6f73d0a01a52f41d751260447b
Spencer Lochne <spencer.lochne@gmail.com>
2025-04-26 20:50:27 -0400

    8l Solution

--- 2-copy-of-code/lesson-08/ecommerce-project/src/pages/home/HomePage.jsx
+++ 2-copy-of-code/lesson-08/ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,20 +1,24 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
 import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
 
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
 
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get('/api/products');
+      const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
       setProducts(response.data);
     };
 
     getHomeData();
-  }, []);
+  }, [search]);
 
   return (
     <>
