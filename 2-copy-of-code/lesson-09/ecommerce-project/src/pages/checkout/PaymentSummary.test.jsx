969214f32a557968a0a23e15568122334cbcd546
Spencer Lochne <spencer.lochne@gmail.com>
2025-05-11 15:04:35 -0400

    9j Solution

--- 2-copy-of-code/lesson-09/ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
+++ 2-copy-of-code/lesson-09/ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
@@ -1,11 +1,16 @@
 import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, useLocation } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import { PaymentSummary } from './PaymentSummary';
+
+vi.mock('axios');
 
 describe('PaymentSummary component', () => {
   let paymentSummary;
   let loadCart;
+  let user;
 
   beforeEach(() => {
     paymentSummary = {
@@ -18,6 +23,7 @@ describe('PaymentSummary component', () => {
     };
 
     loadCart = vi.fn();
+    user = userEvent.setup();
   });
 
   it('displays the correct details', async () => {
@@ -59,5 +65,28 @@ describe('PaymentSummary component', () => {
     expect(
       screen.getByTestId('payment-summary-total')
     ).toHaveTextContent('$52.51');
+  });
+
+  it('places an order', async () => {
+    function Location() {
+      const location = useLocation();
+      return <div data-testid="url-path">{location.pathname}</div>;
+    }
+
+    render(
+      <MemoryRouter>
+        <PaymentSummary
+          paymentSummary={paymentSummary}
+          loadCart={loadCart}
+        />
+        <Location />
+      </MemoryRouter>
+    );
+    const placeOrderButton = screen.getByTestId('place-order-button');
+    await user.click(placeOrderButton);
+
+    expect(axios.post).toHaveBeenCalledWith('/api/orders');
+    expect(loadCart).toHaveBeenCalled();
+    expect(screen.getByTestId('url-path')).toHaveTextContent('/orders');
   });
 });
