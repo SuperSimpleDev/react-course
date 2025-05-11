7ee86c1625e3e60233bad8e45dc36cdc81a31843
Spencer Lochne <spencer.lochne@gmail.com>
2025-05-10 22:17:42 -0400

    9c Solution

--- 2-copy-of-code/lesson-09/ecommerce-project/src/pages/home/Product.test.jsx
+++ 2-copy-of-code/lesson-09/ecommerce-project/src/pages/home/Product.test.jsx
@@ -66,4 +66,11 @@ describe('Product component', () => {
     );
     expect(loadCart).toHaveBeenCalled();
   });
+
+  it('selects a quantity', () => {
+    render(<Product product={product} loadCart={loadCart} />);
+
+    const quantitySelector = screen.getByTestId('product-quantity-selector');
+    expect(quantitySelector).toHaveValue('1');
+  });
 });
