80a29e1486d6bf254a178552f9e04f45d9f0b816
Spencer Lochne <spencer.lochne@gmail.com>
2025-05-28 18:21:18 -0400

    10c Solution

--- 2-copy-of-code/lesson-10/chatbot-project/vite.config.js
+++ 2-copy-of-code/lesson-10/chatbot-project/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // The value should be '/' + your repository name. For example:
+  // if you named your repository "my-repo" the config should be
+  // base: '/my-repo'
+  base: '/chatbot-project'
 })
