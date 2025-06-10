56c033c4bec07eba904c1d2e6797c5c05d3b34af
Spencer Lochne <spencer.lochne@gmail.com>
2025-06-09 20:38:03 -0400

    11c Solution

--- 2-copy-of-code/lesson-11/chatbot-project/vite.config.js
+++ 2-copy-of-code/lesson-11/chatbot-project/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+    },
+  })],
 })
